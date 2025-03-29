b0e2e4b4f26d28f63f51e5da08ddf5e5b701ea41
Edis Rahic <rahic.edis@gmail.com>
2025-03-29 00:21:59 -0400

    Added custom Icons from Material UI, will implement search expansion on click soon

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,15 +1,26 @@
-import react from "react";
+import * as React from "react";
 import Image from "next/image";
-
+import IconButton from '@mui/material/IconButton';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Avatar from '@mui/material/Avatar';
 
 // Need to add yooview logo onto left side of header, search bar will be in middle as input, right side will hold notification buttons
 function Header() {
     return(
         <>
         <div className="flex justify-between">
-        <Image src="/Yooview-logo.png" alt="Logo" width={300} height={300} priority></Image>
-        <div className="justify-center">Search Bar</div>
-        <div className="justify-end">Notifications</div>
+        <Image src="/Yooview-logo.png" alt="Logo" width={300} height={300} priority/>
+        <div className="flex justify-center mt-5">
+            <input type="text" placeholder="Search!" className="border-blue-950 rounded-full w-150 text-start ml-4"/>
+        </div>
+        <div className="flex items-center justify-end gap-6">
+            <IconButton>
+                <DarkModeIcon></DarkModeIcon>
+            </IconButton>
+            <IconButton><NotificationsIcon></NotificationsIcon></IconButton>
+            <IconButton><Avatar></Avatar></IconButton>
+        </div>
         </div>
         </>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Header from "@/app/layout/Header"
 
 export default function Home() {
