91691a15104aa61f2e74e2ba1fe7fa49de76d7d9
Edis Rahic <rahic.edis@gmail.com>
2025-03-30 20:58:41 -0400

    When pressing the menu button (top left) extra options should appear on the sidebar and dissapear when pressed again

--- src/app/layout/SideBar.tsx
+++ src/app/layout/SideBar.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import MenuButton from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
 import HomeIcon from '@mui/icons-material/Home';
@@ -8,18 +9,36 @@ import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
 import HistoryIcon from '@mui/icons-material/History';
 
 function Sidebar() {
-
+    // UseState for when the menu button is pressed, sidebar will expand and provide more options
+    const [menuPressed, setMenuPressed] = useState<boolean>(false)
+    const menuExpand = () => {
+        setMenuPressed(!menuPressed)
+    }
 
 
     return(
         <>
-        <div className="fixed left-0 top-0 h-full ml-6 mt-5"><IconButton><MenuButton></MenuButton></IconButton></div>
+        <div className="caret-transparent h-full">
+        <div className="fixed left-0 top-0 h-full ml-6 mt-5" onClick={menuExpand}><IconButton><MenuButton></MenuButton></IconButton></div>
         <div className="flex-col items-center flex mt-20 ml-5 gap-10">
         <div className="flex flex-col items-center gap-1"><HomeIcon></HomeIcon>Home</div>
         <div className="flex flex-col items-center gap-1"><TheatersIcon></TheatersIcon>Clips</div>
         <div className="flex flex-col items-center gap-1"><AutoAwesomeIcon></AutoAwesomeIcon>Stars</div>
         <div className="flex flex-col items-center gap-1"><EmojiPeopleIcon></EmojiPeopleIcon>You</div>
         <div className="flex flex-col items-center gap-1"><HistoryIcon></HistoryIcon>History</div>
+
+
+        {/*These options will appear when the menu button is pressed (menuPressed = true)*/}
+        <div>
+            {menuPressed && (
+                <>
+                <div className="flex flex-col items-center gap-1">option 1</div>
+                <div className="flex flex-col items-center gap-1">option 2</div>
+                <div className="flex flex-col items-center gap-1">option 3</div>
+                </>
+            )}
+        </div>
+        </div>
         </div>
         </>
     )
