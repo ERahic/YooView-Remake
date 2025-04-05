005150491738cc79b8f72b49f9f8dbc4438ab586
Edis Rahic <rahic.edis@gmail.com>
2025-04-05 19:59:13 -0400

    Fixed the appropriate position for category component, will now put placeholder tags inside component and soon switch to actual data from Youtube Data V3

--- src/app/layout/Category.tsx
+++ src/app/layout/Category.tsx
@@ -1,10 +1,9 @@
 function Category() {
   return (
     <>
-      <div className="flex bg-amber-700 top-30 z-40 debug mt-24 ml-20 rounded-full">
-        Test
+      <div className="fixed flex bg-amber-700 top-24 z-40 debug ml-20 w-full h-16 items-center">
+        CATEGORY TAGS
       </div>
-      ;
     </>
   );
 }
