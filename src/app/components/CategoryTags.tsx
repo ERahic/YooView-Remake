5ad36b2973104a5e43e7dbc520ede5ad489e5135
Edis Rahic <rahic.edis@gmail.com>
2025-04-05 21:25:26 -0400

    Added a list of category tag placeholders attempted to make list of tags appear on category component but failed in the progress, will have to first be able to display tags on category component before beginning to fetch data

---
+++ src/app/components/CategoryTags.tsx
@@ -0,0 +1,23 @@
+// category tags will be made here, first placeholder then actual data being fetched
+interface CategoryTag {
+  id: number;
+  title: string;
+}
+
+function CategoryTags() {
+  // Placeholders for category tags
+  const CategoryPlaceholder: CategoryTag[] = [
+    { id: 1, title: "All" },
+    { id: 2, title: "Gaming" },
+    { id: 3, title: "Science" },
+    { id: 4, title: "Sports" },
+    { id: 5, title: "Movies" },
+    { id: 6, title: "Cooking" },
+    { id: 7, title: "Conspiracy" },
+    { id: 8, title: "Animals" },
+    { id: 9, title: "Live Stream" },
+    { id: 10, title: "Politics" },
+  ];
+}
+
+export default CategoryTags;
--- src/app/layout/Category.tsx
+++ src/app/layout/Category.tsx
@@ -2,7 +2,7 @@ function Category() {
   return (
     <>
       <div className="fixed flex bg-amber-700 top-24 z-40 debug ml-20 w-full h-16 items-center">
-        CATEGORY TAGS
+        TEST TEST TEST
       </div>
     </>
   );
