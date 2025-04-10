71b2722e407b00b4b71307d04d9a06eed64f9711
Edis Rahic <rahic.edis@gmail.com>
2025-04-09 22:38:07 -0400

    After many attempts to get the arrows positioned in the right location and after multiple failed attempts to get the over-extended category tags to be hidden when offscreen, the choice was made to first get the Youtube Data V3 API implemented into the project in order to replace placeholders (category tags and videos) with actual data from Youtube. Added a types folder inside src to help extend Session to accept accessToken as a prop so that [...nextauth].ts can recognize what accessToken is supposed to be. Will implement a login feature for users once OAUTH 2.0 is fully set up for this app, as well as properly fix the front end aspect of the app (i.e category tags and getting them to work with arrow buttons

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+// This will be the authenticator handler for this project.
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+//Provider Configuration, when writing GoogleProvider, it means using OAUTH 2.0 as the provider for this app
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/youtube.readonly",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      //This will save the access token to JWT when the user logs in, if there is an account, it will save the access token into the JWT token
+      if (account) {
+        token.accessToken = account.access_token; // What this does is it saves the access token in JWT token object
+      }
+      return token;
+    },
+    //Send the token to the client session when a session is created
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string; // Attaches the access token to session, and we call token.accessToken as string to let TS know what we want session to be regarding token access
+      return session;
+    },
+  },
+});
